3b4ec7fdac800c7f42fe0010b0cab3cce42e241b
gustavocsak <gustavocsak@gmail.com>
2024-02-17 12:32:47 -0800

    chore: config for globals/tailwind/shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main>
-      <Button>hello</Button>
+    <main className="bg-background w-screen h-screen">
+      <Navbar />
     </main>
   );
 }
