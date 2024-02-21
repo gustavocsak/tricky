23ba07c145ad419c4a744d75e86c31cd5728e24a
gustavocsak <gustavocsak@gmail.com>
2024-02-21 10:16:58 -0800

    feat: add get unique, get all and post project routes

--- app/api/projects/[pid]/route.ts
+++ app/api/projects/[pid]/route.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient()
+import prisma from "../../db";
 
 export async function GET(request: Request, context: any) {
     const { params } = context;
-    return NextResponse.json({
-        id: params.pid
+    const project = prisma.project.findUnique({
+        where: {
+            id: params.pid
+        }
     })
+    return Response.json(project)
 }
--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,8 +1,25 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient()
+import prisma from "../db";
 
 export async function GET() {
-    const allProjects = await prisma.project.findMany()
+    const allProjects = await prisma.project.findMany({
+        include: {
+            tickets: true
+        }
+    })
     return Response.json(allProjects)
+}
+
+export async function POST(request: Request, context: any) {
+    const body = await request.json();
+    
+    const project = await prisma.project.create({
+        data: {
+            ...body,
+        },
+        include: {
+            tickets: true
+        }
+    })
+
+    return Response.json(project)
 }
