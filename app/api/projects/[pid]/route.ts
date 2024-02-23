5aec5276c144343f1aa58b6f183bf93c033c246e
gustavocsak <gustavocsak@gmail.com>
2024-02-22 23:49:36 -0800

    feat: add delete route for project id

--- app/api/projects/[pid]/route.ts
+++ app/api/projects/[pid]/route.ts
@@ -9,3 +9,22 @@ export async function GET(request: Request, context: any) {
     })
     return Response.json(project)
 }
+
+export async function DELETE(request: Request, context: any) {
+    const { params } = context;
+    console.log(params)
+
+    const deleteTickets = await prisma.ticket.deleteMany({
+        where: {
+          projectId: params.pid,
+        },
+    })
+
+    const deleteProject = await prisma.project.delete({
+        where: {
+            id: params.pid
+        }
+    })
+
+    return Response.json(deleteProject)
+}
