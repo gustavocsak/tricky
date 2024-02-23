a159138ba78f91d776340d621becb92c0ec7faec
gustavocsak <gustavocsak@gmail.com>
2024-02-22 23:13:45 -0800

    feat: add project controls

---
+++ components/project-delete.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Button } from './ui/button'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+const ProjectDelete = () => {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button variant='destructive'>Delete Project</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete your project
+                        and remove your data.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
+
+export default ProjectDelete
