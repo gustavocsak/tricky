186f731694aacc12140a0ea2b9695a12bc957bd3
gustavocsak <gustavocsak@gmail.com>
2024-03-01 23:09:36 -0800

    refactor: project-view break down into smaller components

---
+++ components/project-header.tsx
@@ -0,0 +1,18 @@
+import AddTicket from './add-ticket';
+
+interface ProjectHeaderProps {
+    title: string;
+}
+
+export default function ProjectHeader({ title }: ProjectHeaderProps) {
+    return ( 
+        <article className='flex flex-col gap-2'>
+            <section className='flex justify-between items-center py-2'>
+                <h2 className='text-2xl font-bold'>
+                    {title}
+                </h2>
+                <AddTicket />
+            </section>
+        </article>
+    )
+}
--- components/project-view.tsx
+++ components/project-view.tsx
@@ -2,10 +2,8 @@
 import React from 'react'
 import TicketTable from './ticket-table'
 import { useProjectContext } from '../context/project-context';
-import ProjectDelete from './project-delete';
-import ProjectEdit from './project-edit';
-import AddTicket from './add-ticket';
-
+import ProjectHeader from './project-header';
+import ProjectFooter from './project-footer';
 
 export default function ProjectView() {
 	const { currentProject } = useProjectContext();
@@ -15,26 +13,15 @@ export default function ProjectView() {
 		<div className="p-4 w-full flex flex-col gap-4 justify-between">
 			{currentProject ? (
 				<>
-					<article className='flex flex-col gap-2'>
-						<section className='flex justify-between items-center py-2'>
-							<h2 className='text-2xl font-bold'>
-								{currentProject.title}
-							</h2>
-							<AddTicket />
-						</section>
-						
+					<article>
+						<ProjectHeader title={currentProject?.title} />
 						<TicketTable tickets={currentProject?.tickets}/>
 					</article>
 					<article className='flex justify-between items-end'>
-						<section>
-							<h3 className='text-lg font-semibold'>Project details</h3>
-							<p>Author: {currentProject.author}</p>
-							<p>Created: {currentProject.createdAt.substring(0,10)}</p>
-						</section>
-						<div className='space-x-2'>
-							<ProjectDelete />
-							<ProjectEdit />
-						</div>
+						<ProjectFooter 
+							author={currentProject?.author}
+							createdAt={currentProject?.createdAt}
+						/>
 					</article>
 				</>
 			): (
