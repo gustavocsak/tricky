4eae265e827b5b0419fa092e4f33716dd8fe45be
gustavocsak <gustavocsak@gmail.com>
2024-03-22 23:31:31 -0700

    style: import project type from types.ts

--- components/project-list.tsx
+++ components/project-list.tsx
@@ -1,27 +1,16 @@
 'use client'
 import ProjectCard from './project-card';
 import { useProjectContext } from '@/context/project-context';
-
-interface Project {
-    id: string,
-    title: string,
-    author: string,
-    tickets: [],
-    createdAt: string
-}
-
+import { Project } from '@/lib/types';
 interface ProjectListProps {
     data: Project[];
 }
 
-
 export default function ProjectList({ data }: ProjectListProps) {
-    
     const { setProjects } = useProjectContext();
 
     /* To be used in project-view */
     setProjects(data);
-   
     return (
         <ul>
             {data.map((project: Project) => {
